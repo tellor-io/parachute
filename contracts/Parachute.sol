057bf5604961ec0f0b7ce9f7d9157535977fcff0
Tally Wiesenberg <t.j.wies@gmail.com>
2021-06-07 09:38:52 -0400

    51% and failed update

--- contracts/Parachute.sol
+++ contracts/Parachute.sol
@@ -2,30 +2,44 @@
 pragma solidity 0.8.3;
 
 import "hardhat/console.sol";
+import "ITellor.sol";
 
 
 contract Greeter {
-  private address deity;
+  address private multis;
   address tellorMaster;
 
+
   constructor(address _tellorMaster) {
-    deity = //multis account address
+    multis = 0x39e419ba25196794b595b2a595ea8e527ddc9856 ; //multis address
     tellor = _tellorMaster;
   }
 
-  function greet() public view returns (string memory) {
-    return greeting;
+  function rescueFailedUpdate() external {
+    (bool success, bytes memory data) =
+        address(_newTellor).call(
+            abi.encodeWithSelector(0xfc735e99, "") //verify() signature
+        );
+    require(
+        success && abi.decode(data, (uint256)) < CURRENT_VERSION,
+        "new tellor is valid"
+    );
+
+    _rescue();
+
   }
 
-  function setGreeting(string memory _greeting) public {
-    console.log("Changing greeting from '%s' to '%s'", greeting, _greeting);
-    greeting = _greeting;
+  function rescue51PercentAttack(address _tokenHolder) external {
+    require(
+      ITellor(tellorMaster).balanceOf(_tokenHolder) * 100 / ITellor(tellorMaster).totalSupply() >= 51,
+      "attacker balance is < 51% of total supply"
+    );
+
+    _rescue();
   }
 
-  //rescue tellor
-  function _rescue() external {
-    (bool sucess, bytes memory data) = tellor.call(
-      abi.encodeWithSelector(, arg);
-    )
+  //resign control to multis
+  function _rescue() internal {
+    ITellor(tellorMaster).changeDeity(multis);
   }
 }
