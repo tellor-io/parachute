c4521bed4557dc4abe0c9edb9945fc4d8d3d5ce6
Tally Wiesenberg <t.j.wies@gmail.com>
2021-06-11 11:01:52 -0400

    first pass 51 percent

--- test/testParachute.js
+++ test/testParachute.js
@@ -6,7 +6,6 @@ const fs = require('fs')
 let multis, acc1; //accounts
 let parachute, mockOracle, tellor; //contracts
 
-const MY_ADDRESS = "0xA97cd82A05386eAdaFCE2bbD2e6a0CbBa7A53a6c"
 const MULTIS = "0x39E419bA25196794B595B2a595Ea8E527ddC9856"
 const TELLOR = "0x88df592f8eb5d7bd38bfef7deb0fbc02cf3778a0"
 
@@ -14,64 +13,70 @@ const abi = fs.readFileSync('test/abi.json', 'utf8')
 
 beforeEach(async function() {
 
-  await hre.network.provider.request({
-    method: "hardhat_impersonateAccount",
-    params: [MY_ADDRESS]
-  })
-  acc1 = ethers.provider.getSigner(MY_ADDRESS)
+  acc1 = await ethers.getSigner()
 
   await hre.network.provider.request({
     method: "hardhat_impersonateAccount",
     params: [MULTIS]
   })
   multis = ethers.provider.getSigner(MULTIS)
-
   //deploy mock oracle
-  const MockOracle = await ethers.getContractFactory("MockOracle")
+  const MockOracle = await ethers.getContractFactory("contracts/test/MockOracle.sol:MockOracle")
   mockOracle = await MockOracle.deploy() 
-
-  //connect to ITellor to represent Tellor
-  tellor = await ethers.getContractAt("ITellor")
-
-  //upgrade ITellor to MockOracle
-  tellor.connect(multis).changeTellorContract(mockOracle.address)
-
-  //set MockOracle deity to Parachute
-  console.log(1)
+  
+  //deploy parachute
   const Parachute = await ethers.getContractFactory("Parachute")
   parachute = await Parachute.deploy(TELLOR)
-
-  tellor = new ethers.Contract(TELLOR, abi, multis)
-  tellor.connect(multis).changeDeity(parachute.address)
-
-
-  // const Oracle = await ethers.getContractFactory("MockOracle", {
-  //   from: parachute.address
-  // })
-  // oracle = await Oracle.deploy()
+  //connect to ITellor to represent Tellor
+  tellor = await ethers.getContractAt("ITellor", TELLOR)
   
+  //upgrade ITellor to MockOracle
+  await tellor.connect(multis).changeTellorContract(mockOracle.address)
+
+  //set MockOracle deity to Parachute
+  await tellor.connect(multis).changeDeity(parachute.address)
+  
+})
+
+afterEach(async function() {
+  /** multis should be new deity; this means they can change deity */
+  expect(tellor.connect(multis).changeDeity(0))
 })
 
 describe("Tellor Parachute", function() {
 
-
-  
   it("rescue from 51 percent attack", async function() {
 
     //multis can't assume control b/c of regular token holder
     await expect(
-      parachute.rescue51PercentAttack(MY_ADDRESS),
+      parachute.rescue51PercentAttack(acc1.address),
       "parachute took over b/c of regular token holder"
       ).to.be.reverted
 
     //mint total supply to some address
-    oracle.faucet(acc1)
-    
-    //
+    await mockOracle.faucet(acc1.address)
+    let ts = await mockOracle.totalSupply()
+    console.log(Number(ts) / 1E18)
+    console.log(Number(await mockOracle.balanceOf(acc1.address)))
+
+    expect(
+      Number(await mockOracle.balanceOf(acc1.address))
+       * 100 
+       / Number(ts)
+      ).to.be.greaterThan(51) //these are percentages
+
+    parachute.rescue51PercentAttack(acc1.address)
 
   });
 
   it("rescue broken mining", async function() {
+
+    //submit value
+
+    //wait 1 week, expect rescue fails
+
+    //wait another week, expect rescue is allowed
+
 
   })
 
